f484946a3c14f1194b56521f88d4910153024d7c
Torsten Zuther <torsten.zuther@l-p-a.com>
2021-09-15 10:09:10 +0200

    add tests

--- config_test.go
+++ config_test.go
@@ -2,49 +2,64 @@ package main
 
 import (
 	"errors"
+	"io"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+type nonFunctioningReader struct{}
+
+func (e nonFunctioningReader) Read(_ []byte) (n int, err error) {
+	return 0, errors.New("error from reader")
+}
+
+func newStringReader(value string) io.Reader {
+	return strings.NewReader(value)
+}
+
+func newNonFunctioningReader() io.Reader {
+	return nonFunctioningReader{}
+}
+
 func TestParseConfig(t *testing.T) {
 	type testCase struct {
-		input          string
+		reader         io.Reader
 		expectedConfig *config
 		expectedError  error
 	}
 
 	for _, test := range []testCase{
 		{
-			input:          "#123",
+			reader:         newStringReader("#123"),
 			expectedConfig: &config{paths: map[string]string{}},
 			expectedError:  nil,
 		},
 		{
-			input:          "#123\r\n",
+			reader:         newStringReader("#123\r\n"),
 			expectedConfig: &config{paths: map[string]string{}},
 			expectedError:  nil,
 		},
 		{
-			input:          "   \t#    123 # \r\n",
+			reader:         newStringReader("   \t#    123 # \r\n"),
 			expectedConfig: &config{paths: map[string]string{}},
 			expectedError:  nil,
 		},
 		{
-			input:          "   \t#    123 # \r\n   #hh",
+			reader:         newStringReader("   \t#    123 # \r\n   #hh"),
 			expectedConfig: &config{paths: map[string]string{}},
 			expectedError:  nil,
 		},
 		{
-			input: "   \t 123 # \r\n   #hh",
+			reader: newStringReader("   \t 123 # \r\n   #hh"),
 			expectedConfig: &config{paths: map[string]string{
 				"123 #": "123 #",
 			}},
 			expectedError: nil,
 		},
 		{
-			input: "  alias\n  \t\ralias2\t   \r\n    \r\n",
+			reader: newStringReader("  alias\n  \t\ralias2\t   \r\n    \r\n"),
 			expectedConfig: &config{paths: map[string]string{
 				"alias":  "alias",
 				"alias2": "alias2",
@@ -52,7 +67,7 @@ func TestParseConfig(t *testing.T) {
 			expectedError: nil,
 		},
 		{
-			input: "  alias  : \rfile\n  \t\ralias2\t   \r\n ",
+			reader: newStringReader("  alias  : \rfile\n  \t\ralias2\t   \r\n "),
 			expectedConfig: &config{paths: map[string]string{
 				"alias":  "file",
 				"alias2": "alias2",
@@ -60,18 +75,27 @@ func TestParseConfig(t *testing.T) {
 			expectedError: nil,
 		},
 		{
-			input:          "  alias  : \rfile:file\n  \t\ralias2\t   \r\n ",
+			reader:         newStringReader("  alias  : \rfile:file\n  \t\ralias2\t   \r\n "),
 			expectedConfig: nil,
 			expectedError:  errors.New("malformed line"),
 		},
 		{
-			input:          "alias: 123\nalias:321",
+			reader:         newStringReader("alias: 123\nalias:321"),
 			expectedConfig: nil,
 			expectedError:  errors.New("duplicated alias"),
 		},
+		{
+			reader:         newStringReader(""),
+			expectedConfig: &config{paths: map[string]string{}},
+			expectedError:  nil,
+		},
+		{
+			reader:         newNonFunctioningReader(),
+			expectedConfig: nil,
+			expectedError:  errors.New("error from reader"),
+		},
 	} {
-		reader := strings.NewReader(test.input)
-		config, err := parseConfig(reader)
+		config, err := parseConfig(test.reader)
 		assert.Equal(t, config, test.expectedConfig)
 		if test.expectedError != nil {
 			assert.Error(t, err, test.expectedError)
